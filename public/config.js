49256225d79c1e1b8de6bb98f07241ccdbf2e8c4
southernadd-cmyk <southernadd@googlemail.com>
2026-09-15 10:03:18 +0100

    Load v3 lesson styles with frontend config

--- public/config.js
+++ public/config.js
@@ -1,2 +1,8 @@
 // GitHub Pages frontend configuration.
 window.SELAB_API_BASE = 'https://social-engineer-lab-api.mr-adam-clement.workers.dev';
+(() => {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = new URL('./v3.css', document.currentScript.src).href;
+  document.head.appendChild(link);
+})();
